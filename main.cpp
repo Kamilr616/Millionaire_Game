0e85258267369dc26c9a3c3dfd049ac53115d2ae
Kamil Rataj <kamilr616@gmail.com>
2022-05-30 12:25:17 +0200

    question user interface

--- main.cpp
+++ main.cpp
@@ -8,18 +8,54 @@ using namespace std;
 int main()
 {
 
-    int gameStepCounter = 0;
+    int gameStepCounter, i, option, userAns;
+
+    gameStepCounter = 0;
+    i = 0;
+    option = 1;
 
     srand(time(0));
 
     question gameSet[15]; //zestaw pytan do calej gry
 
-    for (int i = 0; i < 15; i++) // losowanie zestawu 15 pytan
+
+    while(option > 0)
     {
-        gameSet[i] = gameSet[i].getQuestions(i+1);
+
+        for (i = 0; i < 15; i++) // losowanie zestawu 15 pytan
+        {
+            gameSet[i] = gameSet[i].getQuestions(i+1);
+
+            
+            system("cls");   // cmd terminal clear
+
+            cout << "Etap : " << (i+1) << endl
+                << "Pytanie: "
+                << gameSet[i].getText() << endl
+                << "Odpowiedzi: " << endl
+                << "1 > " << gameSet[i].getAns1() << endl
+                << "2 > " << gameSet[i].getAns2() << endl
+                << "3 > " << gameSet[i].getAns3() << endl
+                << "4 > " << gameSet[i].getAns4() << endl
+                << "Odpowiedz >> ";
+            cin >> userAns;
+            if(userAns == gameSet[i].getCorr())
+            {
+                cout << "Gratulacje! Poprawna odpowiedz!";            }
+            else
+            {
+                system("cls"); // cmd terminal clear
+                cout << "GAME OVER" << endl;
+                break;
+            }
+            system("PAUSE");
+
+        }
+        cout << "Twoj wynik >> " << (i + 1) << endl
+             << "Kontynuowac? 0. Zakoncz" << endl;
+        cin >> option;
     }
 
 
-    system("PAUSE"); // cmd terminal pause 
     return 0;
 }
