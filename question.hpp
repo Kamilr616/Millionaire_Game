94e9b32aa9a4754bb0ba0ace37b458f5dc30ebfd
Kamil Rataj <kamilr616@gmail.com>
2022-05-30 11:33:02 +0200

    create question.hpp header file

---
+++ question.hpp
@@ -0,0 +1,28 @@
+#include "global.hpp"
+
+using namespace std;
+
+class question 
+{
+
+    private:
+
+        string text,answ1,answ2,answ3,answ4;
+        int correct;
+
+    public:
+
+        question() = default;
+        question(string _text, string _answ1, string _answ2, string _answ3, string _answ4, int _correct);
+
+        string getText();
+        string getAns1();
+        string getAns2();
+        string getAns3();
+        string getAns4();
+        int getCorr();
+        
+        question questionFromLine(string qLine);
+        question getQuestions(int stepCounter);
+
+};
